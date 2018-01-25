d9cec26781387cb4a37f475ec700b2297ff1846b
shenchengren <741141657@qq.com>
2018-01-25 18:09:30 +0800

    0125gongsi

--- src/components/home/shop.vue
+++ src/components/home/shop.vue
@@ -3,7 +3,7 @@
     <div class="shop-content" v-if="content">
       <span class="is-msg" v-show="isTrue">{{msg}}</span>
       <div class="check-all">
-        <input type="checkbox" name="" id="checkAll" @click="clickquan">
+        <input type="checkbox" name="" id="checkAll" @click="clickAll">
         <label for="checkAll">全选</label>
       </div>
       <ul>
@@ -11,349 +11,307 @@
           <p class="title">书名：{{book.name}}<img src="../../assets/images/close.svg" alt="" @click="delFn"></p>
           <div class="content">
             <div class="book-check">
-              <input type="checkbox" name="" id="" :value="book.totnum" v-model="book.checkb" @change="aaa(book)">
+              <input type="checkbox" name="" id="" v-model="book.checkb" @click="checkOne(book,$event)">
             </div>
             <div class="book-img">
               <img :src="book.smallImage" alt="">
             </div>
             <div class="summary"><b>简介：</b>{{book.details && book.details.summary}}</div>
             <div class="pricebox">
-              <span class="prive-add">总价：{{(book.buynumber*book.price).toFixed(2)}}</span>
-              <!-- <input class="prive-add" v-model="nBox[index]">总价：{{(book.buynumber*book.price).toFixed(2)}}</span> -->
-              <!-- <input type="text" class="prive-add" :value="(book.buynumber*book.price).toFixed(2)" :v-model="book[totprice]"> -->
-              <span class="price">单价：￥{{(book.price*1).toFixed(2)}}</span>
+              <span class="prive-add">总价：{{(book.buynumber*book.price)|numberFixed}}</span>
+              <span class="price">单价：￥{{(book.price*1)|numberFixed}}</span>
             </div>
             <div class="numbox">
-              <img src="../../assets/images/minus.svg" alt="" class="minus" @click="minusFn(book,$event)">
-              <!-- <span class="num">1</span> -->
+              <img src="../../assets/images/minus.svg" alt="" class="minus" @click="minusFn(book)">
               <input type="text" class="num" v-model="book.buynumber">
-              <img src="../../assets/images/add.svg" alt="" class="add" @click="addFn(book,$event)">
+              <img src="../../assets/images/add.svg" alt="" class="add" @click="addFn(book)">
             </div>
           </div>
           </li>
       </ul>
-      <div class="totleprice">总价：￥{{totnum}}</div>
+      <div class="totleprice">总价：￥{{totnum|numberFixed}}</div>
     </div>
     <div class="shop-content-null" v-if="!content"><span>购物车空空也！！！</span></div>
   </div>
 </template>
 <script>
-import utils from "@/common/utils"
+import utils from "@/common/utils";
 export default {
-  data(){
+  data() {
     return {
-      content:false,
-      books:[],
+      content: false,
+      books: [],
       msg: "请先登录",
       isTrue: false,
-      totnum:"0.00",
-      nBox:{}
-      // totprice:""
-    }
+      totnum: "0",
+      checkLength: 0
+    };
   },
-  filters:{
-    numberFn(val){
-      // 
+  filters: {
+    numberFixed(val) {
+      return (val * 1).toFixed(2);
     }
   },
   watch: {
-    // '$route'(to, from) {
-    //   console.log("111")
-    //   if (this.getCookie("first_vue_code") != 200) {
-    //     this.$router.push({ path: "/" });
-    //   }
-    // }
-    '$route' (to, from) {
-      console.log("111")
-          
-          }
+    $route(to, from) {}
   },
-  computed: {
-    // 计算是否有错误提示
-    // totprice: function(e) {
-    //   console.log(this.totnum)
-    //   let n=0
-    //   this.totnum.forEach(function(ele,index){
-    //     n+=(+ele);
-    //   })
-    //   // for(i in (this.totnum)){
-    //   //   n+=this.totnum[i]
-    //   // }
-      
-    //   return (n*1).toFixed(2);
-    // } 
-    // quancheck:function(){
-    //   this.books.forEach(function(ele,index){
-    //     if(ele.checkb){
-    //       return true
-    //     }else{
-    //       return false
-    //     }
-    //   })
-    // }  
-  },
-  mounted(){
-    if(utils.getCookie("first_vue_code")==200){
+  computed: {},
+  mounted() {
+    if (utils.getCookie("first_vue_code") == 200) {
       this.getDataCart(utils.getCookie("first_vue_id"));
-    }else{
-      this.content=false
+    } else {
+      this.content = false;
     }
-    
   },
   methods: {
-   getDataCart(userid){
+    getDataCart(userid) {
       let that = this;
-      this.$ajax.post('/api/getcard',{
-        userid:userid
-      }).then(function(response){
-        if(response.data.status==1&&response.data.cart.length>0){
-          that.books=response.data.cart;
-          that.content=true
-        }
-      }).catch(function(error){
-
-      })
-
-    },
-    clickquan(e){
-      var that=this
-      console.log(e.target)
-      if(e.target.checked){
-        this.books.forEach(function(ele,index){
-          ele.checkb=true;
-          that.aaa(ele);
+      this.$ajax
+        .post("/api/getcard", {
+          userid: userid
         })
-      }else{
-        this.books.forEach(function(ele,index){
-          ele.checkb=false;
-          that.aaa(ele);
+        .then(function(response) {
+          if (response.data.status == 1 && response.data.cart.length > 0) {
+            that.books = response.data.cart;
+            that.content = true;
+          }
         })
+        .catch(function(error) {});
+    },
+    clickAll(e) {
+      var that = this;
+      if (e.target.checked) {
+        this.totnum = 0;
+        this.checkLength = this.books.length;
+        this.books.forEach(function(ele, index) {
+          ele.checkb = true;
+          that.totnum = +that.totnum + +(ele.buynumber * ele.price);
+        });
+      } else {
+        this.checkLength = 0;
+        this.books.forEach(function(ele, index) {
+          ele.checkb = false;
+          that.totnum = 0;
+        });
       }
     },
-    aaa(book){
-      // console.log(e.target.checked);
-      if(book.checkb){
-        // book.
-       this.totnum=((+this.totnum)+(+(book.buynumber*book.price))).toFixed(2)
-      }else{
-        this.totnum=((+this.totnum)-(+(book.buynumber*book.price))).toFixed(2)
+    checkOne(book, ele) {
+      let allCheck = document.getElementById("checkAll");
+      let that = this;
+      if (ele.target.checked) {
+        this.checkLength++;
+        this.totnum = +this.totnum + +(book.buynumber * book.price);
+      } else {
+        this.checkLength--;
+        this.totnum = +this.totnum - +(book.buynumber * book.price);
+      }
+      if (this.checkLength == this.books.length) {
+        allCheck.checked = true;
+      } else {
+        allCheck.checked = false;
       }
     },
-    numChange(r){
-      console.log(r)
-    },
-    addFn(book,e){
-      // e.target.previousElementSibling.value++
-      // console.log(this.numBox)
-      book.buynumber++
-      book.totprice=(book.buynumber*book.price).toFixed(2);
-      
-      console.log(book.totprice)
-      if(book.checkb){
-this.totnum=((+this.totnum)+(+book.price)).toFixed(2)
+    addFn(book) {
+      book.buynumber++;
+      book.totprice = book.buynumber * book.price;
+      if (book.checkb) {
+        this.totnum = +this.totnum + +book.price;
       }
-      // console.log(e.target.parentNode.parentNode.firstChild.firstChild);
-      // e.target.parentNode.parentNode.firstChild.firstChild.checked = true;
-      // e.target.pare
-      // this.nBox
-      // console.log(this.nBox)
-      // console.log(book);
-      // console.log(book.totprice);
     },
-    minusFn(book,e){
-      // if(e.target.nextElementSibling.value==1){return}
-      // e.target.nextElementSibling.value--
-      if(book.buynumber==1){return}
-      book.buynumber--
-      book.totprice=(book.buynumber*book.price).toFixed(2);
-      if(book.checkb){
-        this.totnum=((+this.totnum)-(+book.price)).toFixed(2)
+    minusFn(book, books) {
+      if (book.buynumber == 1) {
+        return;
       }
-      
+      book.buynumber--;
+      book.totprice = book.buynumber * book.price;
+      if (book.checkb) {
+        this.totnum = +this.totnum - +book.price;
+      }
     },
-    delFn(){
-      
-        let userId = utils.getCookie("first_vue_id");
-        let bookId = '1001';
-        let that   = this;
-        let url    = '/api/users/'+userId+'/delcart';
-        console.log(url)
-        this.$ajax.put(url,{
-          userId:userId,
-          bookId:bookId
-        }).then(function(response){
-          that.msg = '删除成功';
+    delFn() {
+      let userId = utils.getCookie("first_vue_id");
+      let bookId = "1001";
+      let that = this;
+      let url = "/api/users/" + userId + "/delcart";
+      this.$ajax
+        .put(url, {
+          userId: userId,
+          bookId: bookId
+        })
+        .then(function(response) {
+          that.msg = "删除成功";
           that.isTrue = true;
           setTimeout(function() {
             that.isTrue = false;
           }, 1500);
-        }).catch(function(error){
-          console.log(error)
         })
-      }
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
-  .check-all{
-    margin-top: 35px;
-    padding-left: 15px;
-    box-sizing: border-box;
-    input{
-      height: 16px;
-      width: 16px;
-      line-height: 16px;
-      display: inline-block;
-      vertical-align: middle;   
-    }
-    label{
-      display: inline-block;
-      vertical-align: middle;  
-      padding-left:10px;     
-    }
+.check-all {
+  margin-top: 35px;
+  padding-left: 15px;
+  box-sizing: border-box;
+  input {
+    height: 16px;
+    width: 16px;
+    line-height: 16px;
+    display: inline-block;
+    vertical-align: middle;
   }
-  .book{
-    border: 1px solid #ccc;
-    margin-top: 35px;
+  label {
+    display: inline-block;
+    vertical-align: middle;
+    padding-left: 10px;
   }
-  .shop-content-null{
-    width: 1000px;
-    margin: 0 auto;
-    margin-top: 270px;
-    text-align: center;
-    font-size: 40px;
-    letter-spacing:20px;
-    span{
-      background: red;
-      display: inline-block;
-      text-indent: 32px;
-      padding: 15px 0;
-      border-radius: 40px;
-      box-shadow: 20px 20px 80px #888888;
-      animation: book-null 5s linear infinite;
-    }
-    
-    @keyframes book-null {
-        0% {
-          background: green;
-          transform: rotateX(0deg) rotateY(0deg) translateY(0);
-          // transform: translateY(0);
-        }
-        25% {
-          background: yellow;
-          transform: rotateX(30deg) rotateY(30deg) translateY(70px);
-          // transform: translateY(70px);
-        }
-        50% {
-        background: deeppink;
-        transform: rotateX(0deg) rotateY(0deg) translateY(0);
-        // transform: translateY(0);
-        }
-        75% {
-        background: deepskyblue;
-        transform: rotateX(30deg) rotateY(-30deg) translateY(70px);
-        // transform: translateY(70px);
-        }
-        100% {
-        background: green;
-        transform: rotateX(0deg) rotateY(0deg) translateY(0);
-        // transform: translateY(0);
-        } 
-      }
+}
+.book {
+  border: 1px solid #ccc;
+  margin-top: 35px;
+}
+.shop-content-null {
+  width: 1000px;
+  margin: 0 auto;
+  margin-top: 270px;
+  text-align: center;
+  font-size: 40px;
+  letter-spacing: 20px;
+  span {
+    background: red;
+    display: inline-block;
+    text-indent: 32px;
+    padding: 15px 0;
+    border-radius: 40px;
+    box-shadow: 20px 20px 80px #888888;
+    animation: book-null 5s linear infinite;
   }
 
-  .shop-content{
-    width: 1000px;
-    margin: 0 auto;
-    .content>div{
+  @keyframes book-null {
+    0% {
+      background: green;
+      transform: rotateX(0deg) rotateY(0deg) translateY(0);
+      // transform: translateY(0);
+    }
+    25% {
+      background: yellow;
+      transform: rotateX(30deg) rotateY(30deg) translateY(70px);
+      // transform: translateY(70px);
+    }
+    50% {
+      background: deeppink;
+      transform: rotateX(0deg) rotateY(0deg) translateY(0);
+      // transform: translateY(0);
+    }
+    75% {
+      background: deepskyblue;
+      transform: rotateX(30deg) rotateY(-30deg) translateY(70px);
+      // transform: translateY(70px);
+    }
+    100% {
+      background: green;
+      transform: rotateX(0deg) rotateY(0deg) translateY(0);
+      // transform: translateY(0);
+    }
+  }
+}
+
+.shop-content {
+  width: 1000px;
+  margin: 0 auto;
+  .content > div {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .title {
+    padding: 15px 0px 15px 45px;
+    box-sizing: border-box;
+    font-size: 18px;
+    font-weight: bold;
+    img {
+      width: 20px;
+      height: 20px;
+      vertical-align: middle;
+      float: right;
+      margin-right: 17px;
+      transition: 0.5s;
+      &:hover {
+        transform: rotate(-180deg);
+      }
+    }
+  }
+  .book-check {
+    width: 20px;
+    text-align: center;
+    padding-left: 10px;
+  }
+  .book-img {
+    width: 100px;
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  }
+  .summary {
+    width: 350px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .pricebox {
+    width: 390px;
+    // text-align: center;
+    span {
+      display: inline-block;
+      padding-left: 10px;
+      font-size: 18px;
+      width: 180px;
+    }
+  }
+  .numbox {
+    width: 100px;
+    img {
+      width: 20px;
+      height: 20px;
       display: inline-block;
       vertical-align: middle;
+      border: 1px solid #ccc;
     }
-    .title{
-      padding:15px 0px 15px 45px ;
-      box-sizing: border-box;
-      font-size: 18px;
-      font-weight: bold;
-      img{
-        width: 20px;
-        height: 20px;
-        vertical-align: middle;
-        float: right;
-        margin-right: 17px;
-        transition: 0.5s;
-        &:hover{
-          transform: rotate(-180deg);
-        }
-      }
-    }
-    .book-check{
-      width: 20px;
-      text-align: center;
-      padding-left: 10px;
-    }
-    .book-img{
-      width: 100px;
-      img{
-        width: 100px;
-        height: 100px;
-      }
-    }
-    .summary{
-      width: 350px;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    .pricebox{
-      width: 390px;
-      // text-align: center;
-      span{
-        display: inline-block;
-        padding-left:10px ;
-        font-size: 18px;
-        width: 180px
-      }
-    }
-    .numbox{
-      width: 100px;
-      img{
-        width: 20px;
-        height: 20px;;
-        display: inline-block;
-        vertical-align: middle;
-        border: 1px solid #ccc;
-      }
-      input{
-        display: inline-block;
-        vertical-align: middle;
-        font-size: 22px;
-        // padding: 15px;
-        width: 40px;
-        border: 0;
-        // :disabled
-        text-align: center
-      }
-    }
-    .totleprice{
-      text-align: right;
-      padding-right: 15px;
-      margin-top: 20px;
+    input {
+      display: inline-block;
+      vertical-align: middle;
       font-size: 22px;
-      color: green;
+      // padding: 15px;
+      width: 40px;
+      border: 0;
+      // :disabled
+      text-align: center;
     }
   }
-  .is-msg{
-    position: fixed;
-    top: 40%;
-    left: 50%;
-    background: #000;
-    opacity: 0.5;
-    color: white;
-    font-size: 28px;
-    display: inline-block;
-    text-align: center;
-    padding: 10px;
-    width: 300px;
-    margin-left: -150px;
+  .totleprice {
+    text-align: right;
+    padding-right: 15px;
+    margin-top: 20px;
+    font-size: 22px;
+    color: green;
   }
+}
+.is-msg {
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  background: #000;
+  opacity: 0.5;
+  color: white;
+  font-size: 28px;
+  display: inline-block;
+  text-align: center;
+  padding: 10px;
+  width: 300px;
+  margin-left: -150px;
+}
 </style>
