ec90bf181489a5781ea0670424e7d4ba7e16f41a
shenchengren <741141657@qq.com>
2018-01-26 18:06:12 +0800

    0126gongsi

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+// import ElementUI from "element-ui"
+// import 'element-ui/lib/theme-chalk/index.css'
 // import './cssStyle.scss';
-
+// Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios
 
